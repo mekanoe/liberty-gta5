adb5658f7abcf8792a1d299c60da47c01b88ed31
divinoob <viniaguiar9509@gmail.com>
2017-06-04 03:05:58 -0500

    few more stores three to go

---
+++ server/GTANetwork/resources/liberty/npc-businesses/SChaparralClothes.cs
@@ -0,0 +1,33 @@
+using System;
+using GTANetworkServer;
+using GTANetworkShared;
+
+namespace Liberty.npcBusinesses {
+    class SChaparralClothes : ClothingShop {
+        public SChaparralClothes() : base() {
+            // debug = false;
+
+            shopName = "Discount Store";
+
+            cashierPos = new Vector3(-1102.005f, 2712.014f, 19.10787f); 
+            cashierRot = -134.7596f;
+            cashierModel = "GenHot01AFY";
+
+            shopBoxStart = new Vector3(-1093.394f, 2709.815f, 20f);
+            shopBoxEnd = new Vector3(-1110.616f, 2709.7596f, 20f);
+
+            shopTriggerPos = new Vector3(-1101.395f, 2710.526f, 20f);
+
+            API.onResourceStart += onStart;
+            API.onResourceStart += doors;
+        }
+
+        private void doors() {
+            int d1 = API.exported.doormanager.registerDoor(868499217, new Vector3(-1096.6612548828125, 2705.4458, 19.25779914855957));
+            int d2 = API.exported.doormanager.registerDoor(-1148826190, new Vector3(-1094.9652099609375, 2706.963623046875, 19.2577991485595));
+
+            API.exported.doormanager.refreshDoorState(d1);
+            API.exported.doormanager.refreshDoorState(d2);
+        }
+    }
+}
---
+++ server/GTANetwork/resources/liberty/npc-businesses/SHarmonyClothes.cs
@@ -0,0 +1,32 @@
+using System;
+using GTANetworkServer;
+using GTANetworkShared;
+
+namespace Liberty.npcBusinesses {
+    class SHarmonyClothes : ClothingShop {
+        public SHarmonyClothes() : base() {
+            // debug = true;
+
+            shopName = "Suburban Outfitters";
+
+            cashierPos = new Vector3(612.9198f, 2762.789f, 42.08814f); 
+            cashierRot = -90.45712f;
+            cashierModel = "ShopLowSFY";
+
+            shopBoxStart = new Vector3(613.2281f, 2747.207f, 43f);
+            shopBoxEnd = new Vector3(621.0297f, 2770.496f, 43f);
+
+            shopTriggerPos = new Vector3(614.2259f, 2762.507f, 43f);
+
+            API.onResourceStart += onStart;
+            API.onResourceStart += doors;
+        }
+
+        private void doors() {
+            int d1 = API.exported.doormanager.registerDoor(1780022985, new Vector3(617.2457885742188, 2751.022216796875, 42.757801055908));
+
+            API.exported.doormanager.refreshDoorState(d1);
+        }
+    }
+
+}
