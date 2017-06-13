7670b4bf1b19213957fe754ea81f7a16f6e79c04
kayteh <me@kayteh.com>
2017-06-12 23:27:40 -0500

    cleanup

--- server/GTANetwork/resources/saw/SSpawnManager.cs
+++
@@ -1,3 +0,0 @@
-using System;
-using GTANetworkServer;
-using GTANetworkShared;
--- server/GTANetwork/resources/test/test.js
+++
@@ -1,8 +0,0 @@
-API.onResourceStart.connect(() => {
-	API.sendNotification('i work!')
-	// console.warn('test')
-})
-
-API.onServerEventTrigger.connect((name, args) => {
-	if (name === 'test') API.sendChatMessage('test '+args[0])
-})
